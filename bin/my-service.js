16924d5df7cd73430c5f97db8c9cae65d2ae483c
Eric Henderson <henderea@gmail.com>
2018-06-08 10:35:09 -0400

    make the job name prefix overridable

--- bin/my-service.js
+++ bin/my-service.js
@@ -4,8 +4,14 @@ const chalk = require('chalk');
 const yargs = require('yargs');
 const argv = yargs
     .usage('Usage: $0 command [args]')
-    .wrap(yargs.terminalWidth())
+    .wrap(120)
     .commandDir('../lib/my-service')
+    .option('p', {
+        alias: 'prefix',
+        default: 'com.henderea.',
+        describe: 'specify an alternate prefix',
+        type: 'string'
+    })
     .help('h')
     .alias('h', 'help')
     .argv;

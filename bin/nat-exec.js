5e5149a95aad95bad353c550deabbab29232c1bf
Eric Henderson <henderea@gmail.com>
2019-04-24 11:16:44 -0400

    add nat-exec script

---
+++ bin/nat-exec.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+const arg = require('arg');
+const { orderBy } = require('natural-orderby');
+const shellEscape = require('any-shell-escape');
+const _flattenDeep = require('lodash/flattenDeep');
+const child_process = require('child_process');
+
+const args = arg(
+    {
+        '--command': String,
+        '-c': '--command'
+    },
+    {
+        permissive: true
+    }
+);
+
+let command = args['--command'];
+if(!command) { process.exit(1); }
+
+function _shellEscape(params) {
+    let escaped = [__shellEscape(params)]
+    let flattened = _flattenDeep(escaped)
+    return flattened.join(' ')
+}
+
+function __shellEscape(params) {
+    if(Array.isArray(params)) {
+        return params.map(param => __shellEscape(param))
+    } else if(params == null) {
+        return []
+    } else {
+        return shellEscape(String(params))
+    }
+}
+
+let list = args._;
+
+list = orderBy(list);
+
+if(!command.includes('@@')) {
+    command = `${command} @@`;
+}
+
+command = command.replace(/@@/g, _shellEscape(list));
+
+child_process.execSync(command, { stdio: 'inherit' });

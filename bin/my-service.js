d56f0b91b32fe5774660fd40a5604bdade92b32d
Eric Henderson <henderea@gmail.com>
2019-05-07 13:10:22 -0400

    use ncc for my-service

--- bin/my-service.js
+++ bin/my-service.js
@@ -6,7 +6,17 @@ const path = require('path');
 const argv = yargs
     .usage('Usage: $0 command [args]')
     .wrap(120)
-    .commandDir(path.join(__dirname, '../lib/my-service'))
+    .command(require('../lib/my-service/disable'))
+    .command(require('../lib/my-service/enable'))
+    .command(require('../lib/my-service/log'))
+    .command(require('../lib/my-service/elog'))
+    .command(require('../lib/my-service/tail'))
+    .command(require('../lib/my-service/etail'))
+    .command(require('../lib/my-service/list'))
+    .command(require('../lib/my-service/start'))
+    .command(require('../lib/my-service/stop'))
+    .command(require('../lib/my-service/restart'))
+    .command(require('../lib/my-service/status'))
     .option('p', {
         alias: 'prefix',
         default: 'com.henderea.',

55ceefcae0dc1b806340a3727ef844232ebbbf95
Eric Henderson <henderea@gmail.com>
2019-11-04 13:12:04 -0500

    fix a build error

--- scripts/ncc-build-release-build.sh
+++ scripts/ncc-build-release-build.sh
@@ -3,5 +3,5 @@
 ncc="$(yarn bin ncc)"
 for s; do
     printf '\e[2m$ %s\e[0m\n' "ncc build bin/$s.js -m -q -o dist/$s -e detect-character-encoding"
-    "$ncc" build "bin/$s.js" -m -q -o "dist/$s -e detect-character-encoding"
+    "$ncc" build "bin/$s.js" -m -q -o "dist/$s" -e detect-character-encoding
 done

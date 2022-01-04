cd506302125ed551db5c3279fd0efa91492a1dc5
Eric Henderson <henderea@gmail.com>
2022-01-04 15:07:47 -0500

    support using stdin for xpath-get

--- bin/xpath-get.mjs
+++ bin/xpath-get.mjs
@@ -11,6 +11,8 @@ const serializer = xmldom.XMLSerializer;
 import { HelpTextMaker, style, styles } from '@henderea/simple-colors/helpText.js';
 const { bold, red } = styles;
 
+import { readAll } from '../lib/utils/readAll.mjs';
+
 import arg from 'arg';
 const options = arg(
   {
@@ -44,7 +46,7 @@ const helpText = new HelpTextMaker('xpath-get')
   .params.nl
   .pushWrap(8)
   .dict
-  .key.tab.param('<xml_filename>').value.text('The path to the XML file being examined').end.nl
+  .key.tab.param('<xml_filename>').value.text(`The path to the XML file being examined. Use '-' to read from stdin.`).end.nl
   .key.tab.param('<xpath_expression>').value.text('The xpath expression pointing to the node/value to get').end.nl
   .endDict
   .popWrap()
@@ -63,20 +65,36 @@ ${helpText}`);
   process.exit(1);
 }
 
-options.xml_filename = path.resolve(options._[0]);
+options.xml_filename = options._[0];
 options.xpath_expression = options._[1];
 
-const doc = new dom().parseFromString(fs.readFileSync(path.resolve(options.xml_filename), 'UTF-8'));
-let xpath_expression = options.xpath_expression.replace(/\/([\w\d]+)(?=$|\/)/g, "/*[local-name(.)='$1']");
-const evaluator = xpath.parse(xpath_expression);
-const results = evaluator.evaluate({
-  node: doc,
-  caseInsensitive: true,
-  allowAnyNamespaceForNoPrefix: true
-});
-if(!results || results.length == 0) {
-  process.exit(1);
-} else {
-  const ser = new serializer();
-  console.log(results.nodes.map((r) => ser.serializeToString(r)).join('\n'));
+async function readFile(file) {
+  if(file == '-') {
+    return await readAll(process.stdin);
+  } else if(fs.existsSync(path.resolve(file))) {
+    return fs.readFileSync(path.resolve(file));
+  } else {
+    return null;
+  }
 }
+
+async function run(options) {
+  const fileData = await readFile(options.xml_filename);
+  const doc = new dom().parseFromString(fileData);
+  let xpath_expression = options.xpath_expression.replace(/\/([\w\d]+)(?=$|\/)/g, "/*[local-name(.)='$1']");
+  const evaluator = xpath.parse(xpath_expression);
+  const results = evaluator.evaluate({
+    node: doc,
+    caseInsensitive: true,
+    allowAnyNamespaceForNoPrefix: true
+  });
+  if(!results || results.length == 0) {
+    process.exit(1);
+  } else {
+    const ser = new serializer();
+    console.log(results.nodes.map((r) => ser.serializeToString(r)).join('\n'));
+  }
+  process.exit(0);
+}
+
+run(options);

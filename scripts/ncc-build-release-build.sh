527639e61f5590d28114d53c04a7b45f0d6f91b3
Eric Henderson <henderea@gmail.com>
2023-08-16 14:01:41 -0400

    switch to ncc instead of my-ncc

--- scripts/ncc-build-release-build.sh
+++ scripts/ncc-build-release-build.sh
@@ -2,12 +2,12 @@
 
 source "$(dirname "$0")/_util.sh"
 
-ncc="$(yarn bin my-ncc)"
+ncc="$(yarn bin ncc)"
 for s; do
   s="$(_cleanup_param "$s")"
   if _should_do "$s"; then
     bin_path="$(_get_bin_path "$s")"
-    _pcmd "my-ncc $bin_path -m -q -o dist/$s ${EXCLUDES[*]}"
-    "$ncc" "$bin_path" -m -q -o "dist/$s" ${EXCLUDES[@]}
+    _pcmd "ncc build $bin_path -m -q -o dist/$s ${EXCLUDES[*]}"
+    "$ncc" build "$bin_path" -m -q -o "dist/$s" ${EXCLUDES[@]}
   fi
 done

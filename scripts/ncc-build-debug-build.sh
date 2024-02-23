eefbe06a75ec90537d13159f84c226d316565f7a
Eric Henderson <henderea@gmail.com>
2024-02-23 15:13:30 -0500

    switch to npm

--- scripts/ncc-build-debug-build.sh
+++ scripts/ncc-build-debug-build.sh
@@ -2,12 +2,11 @@
 
 source "$(dirname "$0")/_util.sh"
 
-ncc="$(yarn bin ncc)"
 for s; do
   s="$(_cleanup_param "$s")"
   if _should_do "$s"; then
     bin_path="$(_get_bin_path "$s")"
     _pcmd "ncc" build "$bin_path" -q -o "dist/$s" ${EXCLUDES[*]}
-    "$ncc" build "$bin_path" -q -o "dist/$s" ${EXCLUDES[@]}
+    npx @vercel/ncc build "$bin_path" -q -o "dist/$s" ${EXCLUDES[@]}
   fi
 done
